19f5d1a897976aa10721af95fc9737f7149847ff
Jorge_Sanchez <JorgeSR10@gmail.com>
2026-08-30 18:06:57 +0200

    Incorporacion de match

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 
+opcion = 0
+while(opcion !=6):
 
-print("AGENDA TELEFÓNICA CON PYTHON")
+    print("AGENDA TELEFÓNICA CON PYTHON")
 
-print("Bienvenido a la agenda telefónica, por favor seleccione una opción: \n")
-print("1. Agregar contacto")
-print("2. Buscar contacto")
-print("3. Mostrar todos los contactos")
-print("4. Eliminar contacto")
-print("5. Editar contacto")
-print("6. Salir")
+    print("Bienvenido a la agenda telefónica, por favor seleccione una opción: \n")
+    print("1. Agregar contacto")
+    print("2. Buscar contacto")
+    print("3. Mostrar todos los contactos")
+    print("4. Eliminar contacto")
+    print("5. Editar contacto")
+    print("6. Salir")
+
+    opcion = int(input("Eliga la opción que deseas realizar: "))
+
+
+    match opcion:
+        case 1:
+            print("Has elegido opcion 1")
+        case 2:
+            print("Has elegido opcion 2")
+        case 3:
+            print("Has elegido opcion 3")
+        case 4:
+            print("Has elegido opcion 4")
+        case 5:
+            print("Has elegido opcion 5")
+        case 6:
+            print("Has elegido Salir del Programa")
+
+print("El programa se terminó")
